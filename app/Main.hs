206fe0897f1e8f67d2c53c0014c1e51b3cf56729
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2017-11-11 10:27:22 +0100

    repro

---
+++ app/Main.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import           Data.GI.Base
+import qualified Data.Text    as T
+import           GI.GObject
+import           GI.Gtk       (IsBuilder, ToolButton (..), Window (..))
+import qualified GI.Gtk
+
+main :: IO ()
+main = do
+  _          <- GI.Gtk.init Nothing
+  builder    <- GI.Gtk.builderNewFromFile "app/gui.glade"
+  mainwindow <- builderGetObject Window builder "xcb-vs-gtk-window"
+
+  openButton <- builderGetObject ToolButton builder "open-button"
+  _shid0     <- GI.Gtk.onWidgetButtonReleaseEvent openButton $ \_evbtn ->
+    GI.Gtk.fileChooserNativeNew Nothing
+                                (Just mainwindow)
+                                GI.Gtk.FileChooserActionSave
+                                Nothing
+                                Nothing
+      >>= GI.Gtk.nativeDialogRun
+      >>= print
+      >>  return True
+
+  _shid1     <- GI.Gtk.onWidgetStateFlagsChanged mainwindow print
+
+  _shid2     <- GI.Gtk.onWidgetDestroy mainwindow GI.Gtk.mainQuit
+  GI.Gtk.widgetShowAll mainwindow
+  GI.Gtk.main
+
+builderGetObject
+  :: (GI.GObject.GObject b, IsBuilder a)
+  => (Data.GI.Base.ManagedPtr b -> b)
+  -> a
+  -> T.Text
+  -> IO b
+builderGetObject objectTypeClass builder objectId = do
+  m <- GI.Gtk.builderGetObject builder objectId
+  case m of
+    Nothing -> error $ "Couldn't find GUI object with id " ++ T.unpack objectId ++ " in gui.glade"
+    Just o  -> GI.Gtk.unsafeCastTo objectTypeClass o
